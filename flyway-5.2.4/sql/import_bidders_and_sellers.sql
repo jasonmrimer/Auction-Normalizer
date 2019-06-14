32dba60ca68dc347acdda6e6fb14b5fd6defd099
Jason Rimer <jasonmrimer@gmail.com>
2019-06-13 22:09:02 -0400

    Import users [#166562685]

--- flyway-5.2.4/sql/import_bidders_and_sellers.sql
+++ flyway-5.2.4/sql/import_bidders_and_sellers.sql
@@ -1,1 +1,72 @@
-.IMPORT '/Users/engineer/workspace/cecs535project1/flyway-5.2.4/sql/bidders.dat' user
+drop table temp_user;
+
+create table temp_user
+(
+    id            varchar(64) primary key,
+    rating        integer,
+    location_name varchar(128),
+    country_id    integer,
+    foreign key (country_id) references country (id)
+);
+
+create table temp_user2
+(
+    id          varchar(64) primary key,
+    rating      integer,
+    location_id integer,
+    foreign key (location_id) references location (id)
+);
+
+
+insert into temp_user
+select user.id, user.rating, user.location_name, country.id
+from user
+         left join country on country.name = user.country_name;
+
+insert into temp_user2
+select temp_user.id, temp_user.rating, location.id
+from temp_user
+         left join location on location.name = temp_user.location_name
+    and location.country_id = temp_user.country_id;
+
+drop table user;
+drop table temp_user;
+
+alter table temp_user2
+    rename to user;
+
+alter table user
+    add column seller boolean;
+
+update user
+set seller =
+        (
+            select true
+            from seller
+            where seller.id = user.id
+        )
+where exists
+          (
+              select *
+              from seller
+              where seller.id = user.id
+          );
+
+alter table user
+    add column bidder boolean;
+
+update user
+set bidder =
+        (
+            select true
+            from bidder
+            where bidder.id = user.id
+        )
+where exists
+          (
+              select *
+              from bidder
+              where bidder.id = user.id
+          );
+
+
--- flyway-5.2.4/sql/queries.sql
+++ flyway-5.2.4/sql/queries.sql
@@ -1,14 +1,33 @@
-select count(*)
-from user
-where location_name = 'New York';
-
-
--- query 5
-select count(*)
-
-from seller
-where rating > 1000;
-
+-- 1. Find the number of users in the database.
+-- Answer: 13422
+-- my answer: 13419
 select count(*)
 from user;
 
+-- 2. Find the number of users from New York (i.e., users whose location is the string ”New York”).
+-- Answer: 80
+-- my answer: 80
+select count(*)
+
+from user,
+     (
+         select location.id as lid
+         from location
+         where location.name = 'New York'
+     )
+where user.location_id = lid;
+
+
+-- 5. Find the number of sellers whose rating is higher than 1000.
+-- Answer: 3130
+-- my answer: 3130
+select count(*)
+from user
+where seller = true and rating > 1000;
+
+-- 6. Find the number of users who are both sellers and bidders.
+-- Answer: 6717
+-- my answer: 3442
+select count(*)
+from user
+where seller = true and bidder = true;
