f9fb2e77c82baea24704fe3d4554319eb78bbb57
Jason Rimer <jasonmrimer@gmail.com>
2019-06-15 07:25:23 -0400

    Import bids [#166562701]

---
+++ flyway-5.2.4/sql/V6__Create_Bid_table.sql
@@ -0,0 +1,7 @@
+create table bid
+(
+    item_id integer,
+    user_id varchar(64),
+    time    datetime,
+    amount  float
+);
--- flyway-5.2.4/sql/queries.sql
+++ flyway-5.2.4/sql/queries.sql
@@ -1,8 +1,9 @@
 -- 1. Find the number of users in the database.
 -- Answer: 13422
--- my answer: 13419
+-- my answer: 13419 (pending email with professor whether this is common error)
 select count(*)
 from user;
+
 
 -- 2. Find the number of users from New York (i.e., users whose location is the string ”New York”).
 -- Answer: 80
@@ -18,6 +19,13 @@ from user,
 where user.location_id = lid;
 
 
+-- 4. Find the ID(s) of auction(s) with the highest current price.
+-- Answer: 1046871451
+-- my answer: 1046740686 (reference email with professor, my answer is the correct answer)
+select item_id
+from (select *, max(amount) from bid);
+
+
 -- 5. Find the number of sellers whose rating is higher than 1000.
 -- Answer: 3130
 -- my answer: 3130
@@ -31,3 +39,5 @@ where seller = true and rating > 1000;
 select count(*)
 from user
 where seller = true and bidder = true;
+
+
