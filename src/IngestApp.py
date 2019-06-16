45667a1a1b8f4acb515b24b1cddd761652fafda4
Jason Rimer <jasonmrimer@gmail.com>
2019-06-16 19:29:49 -0400

    Working from command line

--- src/IngestApp.py
+++ src/IngestApp.py
@@ -1,11 +1,46 @@
 from IngestJSON import *
+import logging
+import sys
+import os
+#
+# def main(argv):
+#     if len(argv) < 2:
+#         print >> sys.stderr, 'Usage: python skeleton_json_parser.py <path to json files>'
+#         sys.exit(1)
+#     # loops over all .json files in the argument
+#     for f in argv[1:]:
+#         if isJson(f):
+#             parseJson(f)
+#             print >> "Success parsing " + f
+#
+# if __name__ == '__main__':
+#     main(sys.argv)
 
 
-def main():
+def is_json(f):
+    return len(f) > 5 and f[-5:] == '.json'
+
+
+def main(argv):
     filepaths = []
+    if len(argv) < 2:
+        logging.warning('Usage: python skeleton_json_parser.py <path to json files>')
+        sys.exit(1)
 
-    for i in range(40):
-        filepaths.append(f'/Users/engineer/workspace/cecs535project1/data/json/items-{i}.json')
+    # for f in argv[1:]:
+    #     print(f)
+    #     if is_json(f):
+    #         filepaths.append(f)
+    print(f'arg: {argv[1]}')
+    directory = os.fsencode(argv[1])
+    print(f'dir: {directory}')
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if is_json(filename):
+            filepaths.append(os.path.join(argv[1], filename))
+    # for i in range(40):
+    #     filepaths.append(f'/Users/engineer/workspace/cecs535project1/data/json/items-{i}.json')
 
     ingest_single_value_from_files(
         filepaths,
@@ -51,4 +86,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv)
