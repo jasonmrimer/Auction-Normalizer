ec1b9928d9d85f10060fddc22d6a55cd2313bc92
Jason Rimer <jasonmrimer@gmail.com>
2019-06-17 22:04:09 -0400

    Ready for submission

--- submission/runParser.sh
+++ submission/runParser.sh
@@ -2,10 +2,10 @@
 
 echo -e "**NOTE: This parser requires Python 3\n"
 
-printf 'Setup 1 of 4: \n\tEnter the directory containing Items json files: '
+printf 'Setup 1 of 5: \n\tEnter the directory containing Items json files > '
 read -e -r json_filepath
-echo -e "Setup 2 of 4: \n\tEnter the directory where you will save the dat files:"
-echo -e "\t(or hit ENTER to save in current directory)"
+echo -e "Setup 2 of 5: \n\tEnter the directory to save dat files"
+printf "\t(or hit ENTER to save in current directory) > "
 read -e -r dat_filepath
 
 if ["$dat_filepath" == '']
@@ -24,7 +24,7 @@ do
     done;
 done
 
-echo -e "Setup 3 of 4: \n\tCreating SQLite tables in ebay_db and importing dat files..."
+echo -e "Setup 3 of 5: \n\tCreating SQLite tables in ebay_db and importing dat files..."
 sqlite3 ./ebay_db < commands.sql
 
 sqlite3 ./ebay_db <<END_SQL
@@ -37,8 +37,26 @@ sqlite3 ./ebay_db <<END_SQL
 .import ${dat_filepath}/join_auction_category.dat join_auction_category
 END_SQL
 
-echo -e "Setup 4 of 4: \n\tNormalizing database relationships..."
+echo -e "Setup 4 of 5: \n\tNormalizing database relationships..."
 
 sqlite3 ./ebay_db < normalize.sql
 
-echo -e "Parsing, ingesting, and normalizing complete. Testing queries..."
+echo -e "Step 5: Testing queries...\n"
+echo -e '1. Find the number of users in the database.'
+echo -e "\tCorrect Answer > 13422 | $(sqlite3 ./ebay_db < query1.sql) < My Answer"
+echo -e '2. Find the number of users from New York (i.e., users whose location is the string ”New York”).'
+echo -e "\tCorrect Answer > 80 | $(sqlite3 ./ebay_db < query2.sql) < My Answer"
+echo -e '3. Find the number of auctions belonging to exactly four categories.'
+echo -e "\tCorrect Answer > 8365 | $(sqlite3 ./ebay_db < query3.sql) < My Answer"
+echo -e '4. Find the ID(s) of auction(s) with the highest current price.'
+echo -e "\tCorrect Answer > 1046871451 | $(sqlite3 ./ebay_db < query4.sql)* < My Answer"
+echo -e '5. Find the number of sellers whose rating is higher than 1000.'
+echo -e "\tCorrect Answer > 3130 | $(sqlite3 ./ebay_db < query5.sql) < My Answer"
+echo -e '6. Find the number of users who are both sellers and bidders.'
+echo -e "\tCorrect Answer > 6717 | $(sqlite3 ./ebay_db < query6.sql) < My Answer"
+echo -e '7. Find the number of categories that include at least one item with a bid of more than $100.'
+echo -e "\tCorrect Answer > 150 | $(sqlite3 ./ebay_db < query7.sql) < My Answer"
+echo -e "\n*The given answer to #4 (1046871451) is an incorrect highest price due 1NF db."
+echo -e "Through discussions with Professor Khalefa on June 14, this normalized answer is preferred."
+
+echo -e "\n\nProgram complete. Student: Jason Rimer"
